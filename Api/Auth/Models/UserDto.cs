63068592287e751f81ed799209762a6ebe659d95
oadmin <oadmin@DESKTOP-4HKKJK6>
2026-02-23 15:44:27 +0100

    Add project files.

---
+++ Api/Auth/Models/UserDto.cs
@@ -0,0 +1,22 @@
+﻿namespace Auth.Models
+{
+    public class UserDto
+    {
+        private UserDto(Guid id, string name, string email, string passwordHash)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            PasswordHash = passwordHash;
+        }
+        public Guid Id{ get; set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string PasswordHash { get; private set; }
+        public static UserDto Create (Guid id, string name, string email, string passwordHash)
+        {
+            return new UserDto (id, name, email, passwordHash);
+        }
+
+    }
+}
