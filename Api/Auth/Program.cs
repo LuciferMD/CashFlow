f9fef653bc9ab605b2aaeeaba6753b63fdb0873d
oadmin <dima.fenix@list.ru>
2026-02-24 10:52:45 +0100

    Add context

--- Api/Auth/Program.cs
+++ Api/Auth/Program.cs
@@ -1,3 +1,6 @@
+using Auth.Repositories.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<AuthContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
