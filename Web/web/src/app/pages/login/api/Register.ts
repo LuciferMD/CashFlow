6d1d32142cb0264504ac649d9b6a1bc3d76a5fb2
oadmin <dima.fenix@list.ru>
2026-04-21 10:50:48 +0200

    Create push all configs in separate file

--- Web/web/src/app/pages/login/api/Register.ts
+++ Web/web/src/app/pages/login/api/Register.ts
@@ -1,4 +1,6 @@
-const BASE = "https://localhost:7223";
+import { config } from "../../../../config/api.ts";
+
+const BASE = config.authApiUrl;
 export async function register(userName: string, email: string, password: string) {
     const res = await fetch(`${BASE}/auth/register`, {
         method: "POST",
---
+++ Web/web/src/config/api.ts
@@ -0,0 +1,3 @@
+export const config = {
+    authApiUrl: import.meta.env.VITE_AUTH_API_URL as string,
+} as const;
