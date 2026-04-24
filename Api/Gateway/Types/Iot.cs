76eacdb25f014b077384127860cd198915a3c6f6
oadmin <dima.fenix@list.ru>
2026-04-24 15:11:24 +0200

    Add new hot chocolate service

---
+++ Api/Gateway/Types/Iot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Gateway.Types;
+
+public class Iot
+{
+    public List<IotDevice> Devices { get; set; }
+};
+
+
+public class IotDevice
+{
+    public string Type { get; set; }
+    public string Name { get; set; }
+    public IotPayload Payload { get; set; }
+}
+
+public class IotPayload
+{
+    public int? Co2 { get; set; }
+    public int? Pm25 { get; set; }
+    public int? Humidity { get; set; }
+    public double? Energy { get; set; }
+}
